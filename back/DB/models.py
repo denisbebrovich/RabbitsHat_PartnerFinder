0e994624a165f24786470f452f7e147ff0c59447
denisbebrovich <130680566+denisbebrovich@users.noreply.github.com>
2025-11-20 13:14:45 +0500

    Update models.py

--- back/DB/models.py
+++ back/DB/models.py
@@ -1,1 +1,15 @@
+from sqlalchemy import Column, Integer, String, Float, Text, DateTime, JSON
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
+Base = declarative_base()
+
+class Company(Base):
+  __tablename__='Companies'
+  id = Column(integer, primary_key = True, index = True)
+  name = Column(String(255), nullable = False)
+  industry = Column(String(100))
+  size = Column(String(100))
+  region = Column(String(100))
+  tech_stack = Column(JSON)
+  
