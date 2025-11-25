c2f98dea22625cced65cb6d908a93802caaf4cd5
Azkraft <azanov-2004@mail.ru>
2025-11-26 00:21:41 +0500

    feat: add db endpoint

--- back/fastapi/app/main.py
+++ back/fastapi/app/main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from DB.models import Company, Vacancy
-import json
+from DB.init_database import init_database
+from services.hh_etl import HHExtractor
+import os
 
 app = FastAPI()
 
-db_url = 'postgresql://admin:password@postgres:5432/partner_finder'
+postgres_url = os.environ['POSTGRES_URL']
+db_url = postgres_url + '/partner_finder'
 engine = create_engine(db_url)
 Session = sessionmaker(bind = engine)
 
@@ -15,10 +18,27 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/db/companies")
-async def companies():
+async def db_companies(id=None):
     with Session() as session:
-        return [json.dumps(company) for company in session.query(Company).all()]
+        if id != None:
+            return session.query(Company).filter_by(hh_id=id).all()
+        return session.query(Company).all()
+    
+@app.get("/db/vacancies")
+async def db_vacancies(id=None):
+    with Session() as session:
+        if id != None:
+            return session.query(Company).filter_by(hh_id=id).all()
+        return session.query(Company).all()
+    
+@app.get("/db/init")
+async def db_init():
+    init_database(db_url)
 
+@app.get("/db/fill")
+async def db_fill():
+    extractor = HHExtractor(db_url)
+    extractor.run_etl()
 
 # health
 # docs
