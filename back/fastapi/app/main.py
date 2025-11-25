d1cb029e2b240612b720d378e668446d4839e0d8
Azkraft <azanov-2004@mail.ru>
2025-11-25 22:29:40 +0500

    feat: companies endpoint

--- back/fastapi/app/main.py
+++ back/fastapi/app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from DB.models import Company, Vacancy
+import json
 
 app = FastAPI()
 
+db_url = 'postgresql://admin:password@postgres:5432/partner_finder'
+engine = create_engine(db_url)
+Session = sessionmaker(bind = engine)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/db/companies")
+async def companies():
+    with Session() as session:
+        return [json.dumps(company) for company in session.query(Company).all()]
+
 
 # health
 # docs
